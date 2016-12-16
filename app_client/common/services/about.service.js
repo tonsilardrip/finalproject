2639935361c12924699ec82cd664feeb25c9c8fe
tonsilardrip <cshoemake2226@gmail.com>
2016-12-16 04:52:14 +0000

    Added somethings

---
+++ app_client/common/services/about.service.js
@@ -0,0 +1,13 @@
+angular
+        .module('getfoods')
+        .service('aboutApp', aboutApp);
+
+   
+    function aboutApp(string) {
+        <p>
+        This app just searches a site for a list of foods that you can make. 
+        Lets say you just have hambuger meet in your fridge, you constantly think
+        about what you can make with the item that you have. Well now you don't have to look.
+        You just have to search for what you have and this app will tell you everything about what to make!
+        </p>
+          }
